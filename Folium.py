1cc487199f50c6e4754db25c36dee6974a66451d
alexisgilbert <139812958+alexisgilbert@users.noreply.github.com>
2023-09-04 11:22:02 -0300

    Add files via upload

---
+++ Folium.py
@@ -0,0 +1,9 @@
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+import folium
+
+
+
+map_mexico = folium.Map(location = [23.6345, -102.5528], zoom_start = 4)
+map_mexico.show()
